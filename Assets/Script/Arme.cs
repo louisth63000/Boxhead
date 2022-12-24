30982298303125f21c62ca82284174783c17b4b3
walidassafiri <assafiriwalid@icloud.com>
2022-12-24 12:34:33 +0100

    munition

--- Assets/Script/Arme.cs
+++ Assets/Script/Arme.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class Arme : MonoBehaviour
 {
     public int currentAmmount;
     public int Ammountmax;
+    public TMP_Text ammoText;
     public float rapidfire;
 	public float addfire;
     public float speedbullet;
@@ -38,8 +39,19 @@ public class Arme : MonoBehaviour
         if(Input.GetAxis("Fire1") == 1 &&  rapidfire > 1 && currentAmmount > 0)
 		{
             currentAmmount -=1;
+            UpdateAmmo();
 			rapidfire=0;
 			fire();
+
 		}
+        else if(currentAmmount==0)
+        {
+            currentAmmount=Ammountmax;
+        }
+
     }
+    public void UpdateAmmo()
+	{
+		ammoText.text=currentAmmount.ToString()+" / "+Ammountmax.ToString();
+	}
 }
